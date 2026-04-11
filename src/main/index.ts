aa5aeb9d1a307bc5d5dd792ce5815959da4987a7
AmrDab <amraldabbas19@gmail.com>
2026-04-10 17:55:36 -0700

    v0.4.1: Remove ClawdCursor settings tab, rename to Tools

--- src/main/index.ts
+++ src/main/index.ts
@@ -42,7 +42,7 @@ process.on('uncaughtException', (err) => {
 });
 
 app.whenReady().then(async () => {
-  log.info('ClippyAI starting', { version: '0.4.0' });
+  log.info('ClippyAI starting', { version: '0.4.1' });
   initStartup();
   cleanOldLogs();
 
